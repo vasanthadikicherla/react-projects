33ffea4d0911cecfd636ca06199ec700847d6bc0
vasanthadikicherla <126173570+vasanthadikicherla@users.noreply.github.com>
2023-04-21 13:52:52 +0530

    added register form

--- src/App.js
+++ src/App.js
@@ -13,19 +13,27 @@ function RegistrationForm() {
   return (
     <form onSubmit={handleSubmit}>
       <h2>Register</h2>
+      <div>
       <label>
         Username:
         <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
       </label>
+      </div>
+      <div>
       <label>
         Email:
         <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
       </label>
+      </div>
+      <div>
       <label>
         Password:
         <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
       </label>
+      </div>
+      <div>
       <button type="submit">Submit</button>
+      </div>
     </form>
   );
 }
